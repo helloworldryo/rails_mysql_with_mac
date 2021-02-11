4ef7a98440e2d4b629dde145d9e5d316e5b80a1f
ryohei <ryohei@ryo.local>
2021-02-11 21:41:48 +0900

    11th Feb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+
+  get "signup" => "users#new"
+  
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+  
   get "/" => "home#top"
   get "about" => "home#about"
-  get "users/index" => "users#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
