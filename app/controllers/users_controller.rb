7d675f0a99aca9a00d24c865f894ffa8c996cc1c
ryohei <ryohei@ryo.local>
2021-02-13 21:31:45 +0900

    13th Feb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
   def new
-  
+
   end
+  # Add a new action called "create"
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    redirect_to("/users/#{@user.id}")
+  end
+
 end
