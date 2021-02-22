41f3659c0f6582651f082d28c71d74f577ad7eb7
ryohei <ryohei@ryo.local>
2021-02-22 23:27:30 +0900

    22th Feb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
+
+  # Define the "posts" instance method
+  def posts
+    return Post.where(user_id: self.id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_021512) do
+ActiveRecord::Schema.define(version: 2021_02_20_002858) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
