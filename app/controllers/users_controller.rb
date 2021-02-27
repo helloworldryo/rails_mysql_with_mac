3444d6bb33cb98482cfa391417c3d1474115f637
ryohei <ryohei@ryo.local>
2021-02-27 17:01:33 +0900

    27th Feb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
-  before_action :forbid_login_user, {only: [:top]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
   before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def index
@@ -12,14 +12,16 @@ class UsersController < ApplicationController
   end
 
   def new
-    # Create a new instance of User, then assign it to the @user variable
     @user = User.new
-
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.jpg", password: params[:password])
-    # Redirect to the "Users" page if the @user is valid, and render the "New User" page if it isn't
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg",
+      password: params[:password]
+    )
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully"
@@ -34,15 +36,16 @@ class UsersController < ApplicationController
   end
 
   def update
-  @user = User.find_by(id: params[:id])
-  @user.name = params[:name]
-  @user.email = params[:email]
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
 
     if params[:image]
       @user.image_name = "#{@user.id}.jpg"
       image = params[:image]
       File.binwrite("public/user_images/#{@user.image_name}", image.read)
     end
+
     if @user.save
       flash[:notice] = "Your account has been updated successfully"
       redirect_to("/users/#{@user.id}")
@@ -50,31 +53,37 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+
   def login_form
   end
+
   def login
-    # Get the user with the email and the password, then assign it to the @user variable
-    @user = User.find_by(email: params[:email], password: params[:password])
-    # Redirect to the "Posts" page if the @user exists, and render the "Login Form" page if it isn't
-    if @user
-      # Store the id of the user in session[:user_id]
+    # Rewrite the following line to only use the email to find the user
+    @user = User.find_by(email: params[:email])
+    # Rewrite the if statement using && and the "authenticate" method
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "You have logged in successfully"
       redirect_to("/posts/index")
     else
-      # Define the @error_message variables
       @error_message = "Invalid email/password combination"
-      # Define variables @email and @password
       @email = params[:email]
       @password = params[:password]
       render("users/login_form")
     end
   end
+
   def logout
     session[:user_id] = nil
     flash[:notice] = "You have logged out successfully"
     redirect_to("/login")
   end
+
+  def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
+  end
+
   def ensure_correct_user
     if @current_user.id != params[:id].to_i
       flash[:notice] = "Unauthorized access"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  # Add the has_secure_password method
+  has_secure_password
+
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
 
-  # Define the "posts" instance method
   def posts
     return Post.where(user_id: self.id)
   end
